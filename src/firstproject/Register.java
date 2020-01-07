24177b179d6922c3501becf2a725cdbde3c1ed08
Enes Gönül <enes.gonul@demirorenteknoloji.com>
2020-01-07 14:12:06 +0300

    randomphonenumber

---
+++ src/firstproject/Register.java
@@ -0,0 +1,24 @@
+package firstproject;
+import java.util.Random;
+
+public class Register {
+    public static void main(String[] args) {
+
+        Random telno = new Random();
+        int[] sayilar = new int[11];
+        String start = "053";
+
+
+        for (int j = 0; j <= 100; j++) {
+            System.out.println(j + "telefon numarası");
+            for (int i = 0; i <= 8; i++) {
+                sayilar[i] = telno.nextInt(10);
+            }
+            System.out.println("number : " + start + sayilar[0] + sayilar[1] + sayilar[2] + sayilar[3] + sayilar[4] + sayilar[5] + sayilar[6] + sayilar[7]);
+
+        }
+
+    }
+
+
+}
